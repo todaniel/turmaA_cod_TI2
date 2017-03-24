624a2e645394dfaabc4a516aa14ba2b0a69fc44b
todaniel <aluno18939@ipt.pt>
2017-03-24 16:58:55 +0000

    fase inicial do interface da calculadora

---
+++ 20170324_CalculadoraCompleta/20170324_CalculadoraCompleta/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _20170324_CalculadoraCompleta.Controllers
+{
+    public class HomeController : Controller{
+        // GET: Home
+        public ActionResult Index(){
+            return View();
+
+        }
+
+        // POST: Home
+        [HttpPost]
+        public ActionResult Index(string bt){
+
+            //determinar qual o conteúdo do botão
+
+            return View();
+
+        }
+
+
+
+    }
+}
